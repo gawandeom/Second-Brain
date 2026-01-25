027f84545bdc1e16e9d934e137f5a2091693680a
om gawande <gawandeom11@gmail.com>
2026-01-25 21:15:22 +0530

    added route for the logout

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,11 @@
 import {Router} from "express"
-import { loginUser, registerUser } from "../controller/user.controller"
+import { loginUser, logout, registerUser } from "../controller/user.controller"
 import verifyJwt from "../middleware/auth.middlewares"
 
 const router = Router()
 router.post('/register',registerUser)
 router.post ('/login',loginUser)
-router.get('/verify',verifyJwt,(req,res)=>{
-    res.json({
-        message:"ok"
-    })
-})
+router.post('/logout',verifyJwt,logout)
 
 
 export default router
